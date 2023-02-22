224ce0c38ee3759d1c282554c2841a470a63e216
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-02-22 11:45:21 +0900

    [level 2] Title: 구명보트, Time: 11.58 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people = deque(sorted(people, reverse=True))
+    while len(people) > 1:
+        if people[0] + people[-1] <= limit:
+            answer += 1
+            people.pop()
+            people.popleft()
+        else:
+            answer += 1
+            people.popleft()
+
+    if people:
+        answer += 1
+    return answer
