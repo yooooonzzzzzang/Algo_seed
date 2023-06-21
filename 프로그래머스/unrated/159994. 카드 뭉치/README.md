f2423eb7f2b878c78e792c53f04b9b48c62259a6
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-06-21 17:16:09 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/README.md
+++ 프로그래머스/unrated/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [unrated] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#qna) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,17 +1,15 @@
 def solution(cards1, cards2, goal):
     answer = 'No'
     for i in range(len(goal)):
-        if cards1 or cards2:
-            if cards1 and goal[0] == cards1[0]:
-                print("1번")
-                goal.pop(0)
-                cards1.pop(0)
-            elif cards2 and goal[0] == cards2[0]:
-                print("2번")
-                goal.pop(0)
-                cards2.pop(0)
-            else:
-                break
+    
+        if cards1 and goal[0] == cards1[0]:
+            print("1번")
+            goal.pop(0)
+            cards1.pop(0)
+        elif cards2 and goal[0] == cards2[0]:
+            print("2번")
+            goal.pop(0)
+            cards2.pop(0)
         else:
             break
     if not goal:
