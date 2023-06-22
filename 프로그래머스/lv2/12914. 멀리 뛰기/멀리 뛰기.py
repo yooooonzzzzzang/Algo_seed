4a54c09e4def5d54f6ee8b3751cfbeddbaf0eb6a
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-06-22 21:07:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,7 +1,13 @@
 def solution(n):
     answer = 1
-    dp = [0,1,2,3,5,8]
-    for x in range(6,n+1):
-        dp.append(sum(dp[-2:]))
-    answer = dp[n] %1234567
+    # 1칸 씩 뛰는 방법 + 1 해줘야함
+    # 2를 나눠서 몫과 나머지를 내고 아마 몫 n 이면 나머지 1이나 0 일것
+    mok = n //2
+    na = n % 2
+    for i in range(mok):
+        for j in range(i, -1,-1):
+            print(j)
+    
+    mok = n//2
+    na = n % 2 
     return answer
