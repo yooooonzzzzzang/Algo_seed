506ba0981d88ae602684e228a7724f26dc588be9
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-06-26 14:31:14 +0900

    [level 2] Title: 오픈채팅방, Time: 83.26 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -6,12 +6,12 @@ def solution(record):
         sentence_split = sentence.split()
         if len(sentence_split) == 3:
             dic[sentence_split[1]] = sentence_split[2]
-            
+    
     for sentence in record:
         sentence_split = sentence.split()
-        if sentence_split[0] == 'Enter':
-            answer.append('%s님이 들어왔습니다.' %dic[sentence_split[1]])
-        elif sentence_split[0] == 'Leave':
-            answer.append('%s님이 나갔습니다.' %dic[sentence_split[1]])
-            
+        if len(sentence_split) == 2:
+            answer.append(f'{dic[sentence_split[1]]}님이 나갔습니다.')
+        else:
+            if sentence_split[0] =="Enter":
+                answer.append(f'{dic[sentence_split[1]]}님이 들어왔습니다.')
     return(answer)
