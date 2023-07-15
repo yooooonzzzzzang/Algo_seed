d669ebd6b3d7179434ab7b9e757e271de5d022e8
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-15 18:37:04 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,3 +1,12 @@
+from collections import Counter
 def solution(X, Y):
-    answer = ''
-    return answer
+    x_count = Counter(X)
+    y_count = Counter(Y)
+    k = list((x_count&y_count).elements())
+    k.sort(reverse=True)
+    if not k :
+        return "-1"
+    if k[0] == "0":
+        return "0"
+    return ''.join(k)
+    
