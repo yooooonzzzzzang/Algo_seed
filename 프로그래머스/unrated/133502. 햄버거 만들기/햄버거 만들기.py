16901f818f1c51ad07036e65256610f4a45ca6a5
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-21 14:55:24 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,14 @@
+def solution(ingredient):
+    answer = 0
+    doma = []
+    for i in range(len(ingredient)):
+        doma.append(ingredient[i])
+        if len(doma) >= 4:
+            if doma[-4:] == [1,2,3,1]:
+                answer += 1
+                # doma = doma[:-4]
+                del doma[-1]
+                del doma[-1]
+                del doma[-1]
+                del doma[-1]
+    return answer
