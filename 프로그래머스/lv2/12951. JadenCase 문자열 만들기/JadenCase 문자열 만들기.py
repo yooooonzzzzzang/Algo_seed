d73a04b0f5f8096a7aed77fe2bb83caea8a700f0
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-21 09:30:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,13 +1,16 @@
 def solution(s):
-    temp = ''
-    for i in range(len(s)):
-        temp += s[i].lower()
-    answer = ''
-    answer+= temp[0].upper()
-    for i in range(1,len(temp)):
-        if temp[i-1] == ' ' and temp[i].islower() == True:
-            answer += temp[i].upper()
-        else:
-            answer += temp[i]
-
-    return answer
+    s = s.lower()
+    arr = []
+    tmp = 0 
+    # 이전 문자가 공백이고 다음은 공백이 아닐때까지 자르기
+    for i in range(len(s)-1):
+        if s[i] == ' ' and s[i+1] != ' ':
+            arr.append(s[tmp:i+1])
+            tmp = i+1
+    # 마지막 문자열 추가
+    arr.append(s[tmp:])
+    
+    for i in range(len(arr)):
+        if not arr[i][0].isnumeric():  # 맨 앞 글자가[0] 숫자가 아니면
+            arr[i] = arr[i].title()    # 대문자
+    return ''.join(arr)        
--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
