9f6b490548f340535bbc0521a256ceaecd4b0f0d
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-24 09:15:52 +0900

    [level 2] Title: 올바른 괄호, Time: 8.79 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,15 @@
 def solution(s):
     stack = []
+    # ( 먼저 들어가야 올바른 괄호
     for i in s:
         if i == ')':
+            # ) 부터 시작해서 잘못됨
             if not stack:
                 return False
-            stack.pop()
-        else :stack.append(i)
-    if stack:
-        return False
-    return True
+            elif stack[-1] == '(':
+                stack.pop()
+        else:
+            stack.append(i)
+    if not stack:
+        return True
+    else: return False
