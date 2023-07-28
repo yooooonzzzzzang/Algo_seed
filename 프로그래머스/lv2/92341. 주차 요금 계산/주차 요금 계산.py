5fa3efc6260edafe924a73165eff22b8a9978d63
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-28 09:29:33 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,42 +1,29 @@
-import math
-
 def solution(fees, records):
     basic_minutes, basic_fee, unit_minutes, unit_fee = fees[0], fees[1], fees[2],fees[3]
-    answer =[]
     dic = {}
-    dic2 = {}
+    cars =[]
     for i in records:
         time, car_num, content = i.split()
+        cars.append(car_num)
         # 입차
         if content == "IN":
-            dic[car_num]= [time, "23:59"]
+            dic[car_num] = [time, "23:59"]
         # 출차
         else:
             dic[car_num][1] = time
-            # 계산 
-            p_h, p_m = map(int, dic[car_num][1].split(":"))
-            c_h, c_m = map(int, dic[car_num][0].split(":"))
-            total = (p_h*60 + p_m) -(c_h *60 + c_m)
-            if car_num in dic2:
-                dic2[car_num] += total
-            else:
-                dic2[car_num] = total
-            del dic[car_num]
-    for i in dic.keys():
+            
+    print(dic)
+    answer = []
+    # 기본 시간 이하면 기본요금, 초과시 단위시간마다 단위요금 청구 
+    # 초과시간이 나눠지지 않으면 올림
+    # 차량번호가 작은 자동차부터 주차요금 계산 하기 
+    for i in sorted(dic.keys()):
+        print(dic[i])
         p_h, p_m = map(int, dic[i][1].split(":"))
         c_h, c_m = map(int, dic[i][0].split(":"))
         total = (p_h*60 + p_m) -(c_h *60 + c_m)
-        if i in dic2:
-            dic2[i] += total
-        else:
-            dic2[i] = total
-    print(dic2)
-    for i in sorted(dic2.keys()):
-        total = dic2[i]
         if total > basic_minutes:
-            k = math.ceil((total-basic_minutes)/unit_minutes)
-            answer.append(basic_fee+k*unit_fee)
+            pass
         else:
             answer.append(basic_fee)
-        
     return answer
