2e14d0bbad6cf518e6f0534365d120f2637c1228
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-07-29 17:34:32 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,44 +1,17 @@
 def solution(survey, choices):
     answer = ''
-    dic = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
-    for idx, i in enumerate(survey):
-        if choices[idx] == 1:
-            dic[str(i[0])] += 3
-        elif choices[idx] == 2:
-            dic[str(i[0])] += 2
-        elif choices[idx] == 3:
-            dic[str(i[0])] += 1
-        elif choices[idx] == 4:
-            pass
-        elif choices[idx] == 5:
-            dic[str(i[1])] += 1
-        elif choices[idx] == 6:
-            dic[str(i[1])] += 2
-        elif choices[idx] == 7:
-            dic[str(i[1])] += 3
-
-    if dic['R'] > dic['T']:
-        answer += 'R'
-    elif dic['R'] == dic['T']:
-        answer += 'R'
-    else:
-        answer += 'T'
-    if dic['C'] > dic['F']:
-        answer += 'C'
-    elif dic['C'] == dic['F']:
-        answer += 'C'
-    else:
-        answer += 'F'
-    if dic['J'] > dic['M']:
-        answer += 'J'
-    elif dic['J'] == dic['M']:
-        answer += 'J'
-    else:
-        answer += 'M'
-    if dic['A'] > dic['N']:
-        answer += 'A'
-    elif dic['A'] == dic['N']:
-        answer += 'A'
-    else:
-        answer += 'N'
+    score  = [0,3,2,1,0,1,2,3]
+    dic = {"R":0, "T":0, "C":0,"F":0, "J":0,"M":0,"A":0,"N":0}
+    for i,v in enumerate(survey):
+        check = choices[i]
+        if check >=4:
+            dic[v[1]] += score[check]
+        else:
+            dic[v[0]] += score[check]
+    arr =[("R","T"),("C","F"),("J","M"),("A","N")]
+    for i in arr:
+        if dic[i[0]] >= dic[i[1]]:
+            answer+=i[0]
+        else:
+            answer+=i[1]
     return answer
