f33ee2d0f2749bca0ac36d4ffe02df97d7889b9f
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-08-01 09:49:32 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,14 +1,15 @@
 def solution(brown, yellow):
-    answer = [0,0]
-    s = yellow + brown                # 전체 면적
-    for i in range(s-1, 0, -1):
-        if s % i:
-            pass
-        height = s // i;
-        y = (i -2) * (height -2)
-        b = s - y
-        if y == yellow and b == brown:
-            answer[0] = i
-            answer[1] = height
-            break
-    return answer
+    answer = []
+    # 방법1) 이 둘을 더한 직사각형을 먼저 구해서 될 수 있는 가로 세로를 전부 검증하는 방법 
+    total = brown + yellow
+    # 약수 구해주기 (가로 >= 세로)
+    divisor = []
+    for i in range(1,total//2+1):
+        mok = total // i
+        na = total % i
+        if na == 0 and mok >= i:
+            divisor.append([mok , i])
+    # 가로 -2 * 세로 -2 == yellow: 정답
+    for width, height in divisor:
+        if (width-2) * (height -2) == yellow:
+            return [width, height]
