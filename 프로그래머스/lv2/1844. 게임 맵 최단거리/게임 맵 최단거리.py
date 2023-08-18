22092af94fcf5d2ad63665b0b8cf27fa96124965
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-08-18 16:19:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,22 @@
+
 from collections import deque
-
 def solution(maps):
-    n, m = len(maps), len(maps[0])
-    visited = [[False] * m for _ in range(n)]
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    if n == 1 and m == 1:
-        return 1
-    queue = deque()
-    queue.append((0,0))
-
-    while queue:
-        x, y = queue.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            # 미로 범위 안에 있고 아직 방문 안했고 1이면:
-            if 0 <= nx < n and 0<= ny <m and not visited[nx][ny] and maps[nx][ny] == 1:
+    answer = 0
+    dx = [0,0,-1,1]
+    dy = [1,-1,0,0]
+    v = [[0] * len(maps[0]) for _ in range(len(maps))]
+    
+    q = deque()
+    v[0][0] = 1
+    q.append((0,0))
+    while q:
+        x,y = q.popleft()
+        if x == len(maps)-1 and y == len(maps[0])-1:
+            return maps[len(maps)-1][len(maps[0])-1]
+        for dir in range(4):
+            nx, ny = dx[dir] + x , dy[dir] + y 
+            if 0<= nx <len(maps) and 0<= ny <len(maps[0]) and not v[nx][ny] and maps[nx][ny] ==1:
+                v[nx][ny] = True
+                q.append((nx,ny))
                 maps[nx][ny] = maps[x][y] + 1
-                visited[nx][ny] =True
-                queue.append((nx,ny))
-    if maps[n-1][m-1] > 1:
-        return maps[n-1][m-1]
-    else:
-        return -1
-
-
+    return -1
