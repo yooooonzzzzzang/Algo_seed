2ea613f762b9d7a93331c5122eed5dcccad83fe7
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-08-24 21:50:14 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,15 +1,29 @@
-
-
-
-def solution(n, lost, reserve):
-    answer = 0
-    set_reserve = set(reserve) - set(lost)
-    set_lost = set(lost)-set(reserve)
+# def solution(n, lost, reserve):
+#     answer = 0
+#     # 1. 여벌 체육복 - 도난
+#     for i in lost:
+#         if i in reserve:
+#             del reserve[reserve.index(i)]
+#             del lost[lost.index(i)]
     
-    for i in set_reserve:
-        if i-1 in set_lost:
-            set_lost.remove(i-1)
-        elif i+1 in set_lost:
-            set_lost.remove(i+1)
+#     # 2. 최댓값 디용 앞에 있는 애를 빌려줘야할듯
+#     for i in range(len(lost)):
+        
+#     return answer
+
+def solution(n, lost, reserve): 
+    answer = 0 
+    
+    reserve_del = set(reserve)-set(lost) 
+    lost_del = set(lost)-set(reserve) 
+    
+    for i in reserve_del: 
+        if i-1 in lost_del: 
+            lost_del.remove(i-1) 
             
-    return n-len(set_lost)
+        elif i+1 in lost_del: 
+            lost_del.remove(i+1) 
+            
+    answer = n - len(lost_del)
+    
+    return answer
