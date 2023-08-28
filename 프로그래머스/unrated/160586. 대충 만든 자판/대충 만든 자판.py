4bc03cc428a7dc15459cf4d29ee97720052d3aa1
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-08-28 13:02:18 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.65 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,21 +1,4 @@
 def solution(keymap, targets):
     answer = []
-    dic = {}
-
-    for i in range(len(keymap)):
-        for j in range(len(keymap[i])):
-            if not dic.get(keymap[i][j]):
-                dic[keymap[i][j]] = j+1
-            else: dic[keymap[i][j]] = min(j+1, dic[keymap[i][j]])
-    for i in targets:
-        flag = True
-        cnt = 0
-        for j in i:
-            if not dic.get(j):
-                answer.append(-1)
-                flag = False
-                break
-            else: cnt += dic[j]
-        if flag == True:
-            answer.append(cnt)
+    print(keymap)
     return answer
