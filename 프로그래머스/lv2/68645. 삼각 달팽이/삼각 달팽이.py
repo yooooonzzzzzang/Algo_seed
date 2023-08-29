7116921022fd11cb65ff2b309df0835ea8b9a4b7
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-08-29 16:31:51 +0900

    [level 2] Title: 삼각 달팽이, Time: 2119.58 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = [[0 for j in range(1, i+1)] for i in range(1, n+1)]
+    x,y = -1,0
+    num = 1
+    for i in range(n):
+        for j in range(i,n):
+            if i % 3 == 0:
+                x += 1 # 아래
+            elif i % 3 == 1:
+                y += 1 # 오른쪽
+            else:      # 위
+                x -= 1
+                y -= 1 
+            answer[x][y] = num
+            num += 1
+    return sum(answer,[])
