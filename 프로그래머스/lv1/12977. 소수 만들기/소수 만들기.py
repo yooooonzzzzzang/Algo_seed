e84c6a7d43178bd0a284860dcf96956645a6a634
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-09-06 17:45:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,22 +1,22 @@
-from itertools import combinations
 
-def is_prime_num(n):
-    if n <2:
+
+def is_prime(n):
+    if n == 1:
         return False
     if n==2 or n==3:
         return True
-    if n%2==0:
+    if n % 2 == 0:
         return False
-    for i in range(3, int(n**1//2),2):
-        if n%i == 0:
+    for m in range(3,int(n**0.5)+1):
+        if n%m==0:
             return False
     return True
+    
 def solution(nums):
-    answer = 0 
-    arr = [sum(i) for i in list(combinations(nums,3))]
-    # 3개 조합
-    # 소수 판별
-    for n in arr:
-        if is_prime_num(n):
-            answer+=1
+    answer = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if is_prime(nums[i]+nums[j]+nums[k]):
+                    answer += 1
     return answer
