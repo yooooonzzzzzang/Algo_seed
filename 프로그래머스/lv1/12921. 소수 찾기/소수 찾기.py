8e64ed357a458c6226c20f88a44c52294d1da278
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-09-07 15:24:29 +0900

    [level 1] Title: 소수 찾기, Time: 1762.16 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,9 +1,31 @@
+# def solution(n):
+#     answer = 0
+#     ch = [0] * (n+1)
+#     for i in range(2, n+1):
+#         if ch[i] == 0:
+#             answer += 1
+#             for j in range(i, n+1, i):
+#                 ch[j] = 1
+#     print(ch)
+#     return answer
+
+
+def is_prime(k):
+    if k <2:
+        return False
+    if k==2 or k == 3:
+        return True
+    if k % 2 == 0:
+        return False
+    for i in range(3, int(k**0.5)+1, 2):
+        if k % i == 0:
+            return False
+    return True
+
+
 def solution(n):
     answer = 0
-    ch = [0] * (n+1)
-    for i in range(2, n+1):
-        if ch[i] == 0:
+    for i in range(2,n+1):
+        if is_prime(i):
             answer += 1
-            for j in range(i, n+1, i):
-                ch[j] = 1
     return answer
