d9de04b7a9317b55a31abdb72c4b2696606e95a7
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-09-16 00:30:04 +0900

    [level 2] Title: 택배상자, Time: 335.26 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,19 @@
+from collections import deque
+def solution(order):
+    order = deque(order)
+    answer = 0
+    n = len(order)
+    temp = []
+    for i in range(1, n+1):
+        if i <= order[0]:
+            temp.append(i)
+
+        while True:
+            if temp and temp[-1] == order[0]:
+                temp.pop()
+                order.popleft()
+                answer += 1
+            else:
+                break
+
+    return answer
