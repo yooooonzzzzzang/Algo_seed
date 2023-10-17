44f9f8594e353bed1897e68ab4ec8b2b4792f638
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-10-17 19:45:26 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,15 +1,18 @@
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-answer = 0
-def recur(idx, total):
+
+def recur(idx):
     global answer
 
-    if idx > n-1:
-        if idx > n: return
-        answer = max(answer, total)
-        return
-    recur(idx+arr[idx][0], total+arr[idx][1])
-    recur(idx+1, total)
-
-recur(0,0)
-print(answer)
+    if idx == n:
+        return 0
+    if idx > n :
+        return -999999999999999
+    if dp[idx] != -1: # 이미 저장되었다면
+        return dp[idx]
+    # 상담을 받거나 안받거나 그중에서 더 많은 돈을 버는 경우를 내 dp 테이블에 저장
+    dp[idx] =  max(recur(idx+arr[idx][0])+ arr[idx][1], recur(idx+1))
+    return dp[idx]
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [-1] * (n+1)
+answer = 0
+print(recur(0))
