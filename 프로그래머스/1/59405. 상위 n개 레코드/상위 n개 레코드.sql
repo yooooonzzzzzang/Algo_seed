b693e17839fc975dc625b9c4547878916ec80f5e
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2023-11-09 21:40:08 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,2 +1,3 @@
 -- 코드를 입력하세요
-SELECT NAME FROM ANIMAL_INS order by datetime limit 1;
+# SELECT NAME FROM ANIMAL_INS order by datetime limit 1;
+SELECT NAME from ANIMAL_INS where DATETIME = (select min(DATETIME) from ANIMAL_INS)
