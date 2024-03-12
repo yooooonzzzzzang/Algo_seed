ebb566793bab0674cba0c97a1d7b75aab29a000c
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-03-12 22:59:33 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,22 +1,20 @@
-
-
 def solution(genres, plays):
-# 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
-# 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-# 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
     answer = []
-    dic = dict()
-    dic2 = dict() # 재생된 노래수 확인용
-    for i in range(len(genres)):
-        if genres[i] in dic:
-            dic[genres[i]].append([i, plays[i]])
-            dic2[genres[i]] += plays[i]
-        else: 
-            dic[genres[i]] = [[i,plays[i]]]
-            dic2[genres[i]] = plays[i]
-
-    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True): # 많이 재생된 장르 장렬
-        for (i, p) in sorted(dic[k], key=lambda x:x[1], reverse=True)[:2]: # 재생 횟수로 정렬, 두개 슬라이싱
-            answer.append(i)
-
+    dic = {}
+    for i, v in enumerate(zip(genres, plays)):
+        if v[0] not in dic:
+            dic[v[0]] = {
+                'plays' : {
+                    i:v[1]
+                },
+                'total_play' : v[1]
+            }
+        else:
+            dic[v[0]]['plays'][i] = v[1]
+            dic[v[0]]['total_play'] += v[1]
+    k = sorted(dic.values(), key =lambda x:-x['total_play'])
+    for item in k:
+        tmp = sorted(item['plays'].items(), key = lambda x : (-x[1], x[0]))[:2]
+        for i in tmp :
+            answer.append(i[0])
     return answer
