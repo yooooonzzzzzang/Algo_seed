9eb8b173836e7c62be40f4d73cee7df8b9f85708
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-03-13 21:22:27 +0900

    [level 2] Title: 모음 사전, Time: 3.64 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,3 +1,23 @@
+def solution(word):
+    arr = ['A','E','I','O','U']
+    cnt = 0
+    answ = 0
+    def dfs(tmp):
+        nonlocal cnt
+        nonlocal answ
+        if len(tmp) > 5:
+            return 
+        cnt += 1
+        if tmp == word:
+            answ = cnt
+        
+        for i in arr:
+            dfs(tmp + i)
+                
+    dfs('')
+    return answ-1         
+
+'''
 from itertools import product
 def solution(word):
     answer = 0
@@ -9,3 +29,4 @@ def solution(word):
     tmp.sort()
     
     return tmp.index(word) + 1
+'''
