f49be45c66f65fa57b87b97963ee8039ee0e1482
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-03-18 17:24:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -8,26 +8,29 @@ def solution(n, wires):
         arr[bb].append(aa)
         
     def dfs(node, v):
+        global cnt
         v[node] = 1
-        cnt = 1
+        cnt += 1
         for i in arr[node]:
             if not v[i] :
-                cnt += dfs(i, v)
-        return cnt
+                dfs(i, v)
                 
         
     def cut(wire):
+        global cnt
         v = [0] * (n+1)
         a,b = wires[wire][0], wires[wire][1]
         # 제거 
         arr[a].remove(b)
         arr[b].remove(a)
         # count 
-        cnt = abs(dfs(a, v)-dfs(b, v))
+        cnt = 0
+        dfs(a, v)
+        j = abs(n - cnt - cnt)
         
         arr[a].append(b)
         arr[b].append(a)
-        return  cnt
+        return  j
     # 전력망 나누기
     for i in range(n-1):
         t = cut(i)
