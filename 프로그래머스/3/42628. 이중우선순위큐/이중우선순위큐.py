900e13240cddc2c50f5aa6ff818ab2d8d5ee71c2
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-03-19 16:23:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,18 @@
+import heapq
+def solution(operations):
+    min_heap = []
+    for op in operations:
+        m, n = op.split(" ")
+        n = int(n)
+        if m == 'I':
+            heapq.heappush(min_heap, n)
+        else:
+            if min_heap:
+                if n > 0:
+                    # 최댓값 삭제
+                    k = heapq.nlargest(1, min_heap)
+                    min_heap.remove(int(k[0]))
+                else:
+                    # 최솟값 삭제
+                    heapq.heappop(min_heap)
+    return [max(min_heap), min(min_heap)] if min_heap else [0,0]
