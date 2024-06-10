8230959e4a567ccb056155c3a9607b2bea6d64a4
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-06-10 15:52:59 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,14 +1,21 @@
-n, m = map(int,input().split())
-arr =sorted(map(int, input().split()))
-tmp = []
-def recur(start):
-    if len(tmp) == m:
-        print(*tmp)
+n,m = map(int, input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+
+ans = [0] * m
+v = [0] * n
+
+def dfs(x, start):
+    if x == m:
+        print(*ans)
         return
     for i in range(start, n):
-        if arr[i] not in tmp:
-            tmp.append(arr[i])
-            recur(i+1)
-            tmp.pop()
-#
-recur(0)
+        if not v[i]:
+            ans[x] = arr[i]
+            v[i] = 1
+            dfs(x+1, i)
+            v[i] = 0
+            ans[x] = 0
+
+
+dfs(0,0)
