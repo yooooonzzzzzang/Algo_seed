80409db0f68a78c33ed3debe2f4ebb966da461bb
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-03 22:19:47 +0900

    Title: 독서실의 거리두기 5, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,19 +1,20 @@
 n = int(input())
 arr = list(map(int,input()))
-# 0 ~ n +1 까지 직접 넣어보고 최대 거리를 계산 
-prev = 0
+
 ans = []
-for i in range(n+1):
-    arr.insert(i,1)
-    tmp = []
-    for j in range(n+1):
-        if arr[j] == 1:
-            tmp.append(j)
-    max_a = 21
-    for k in range(1,len(tmp)):
-        max_a = min(tmp[k] - tmp[k-1], max_a)
-    ans.append(max_a)
-
-
-    del arr[i]
+for i in range(n):
+    max_ans = 21
+    if arr[i] == 0:
+        arr[i] = 1
+        
+        index_i = []
+        
+        for j in range(n):
+            if arr[j] == 1:
+                index_i.append(j)
+        for j in range(1,len(index_i)):
+            max_ans = min(index_i[j] - index_i[j-1], max_ans)
+        
+        ans.append(max_ans)
+        arr[i] = 0
 print(max(ans))
