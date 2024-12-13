35332267d723bfe0c769fec91537f2941e730559
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-13 14:28:22 +0900

    Title: A, B, C, D 찾기 2, Time: 88ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241213/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -0,0 +1,26 @@
+arr = list(map(int,input().split()))
+for A in range(15):
+    for B in range(15):
+        for C in range(15):
+            for D in range(15):
+                if A == B or A == C or A == D or B == C or B== D:
+                    continue
+                a = arr[A]
+                b = arr[B]
+                c = arr[C]
+                d = arr[D]
+                tmp = [a,b,c,d]
+                tmp.append(a + b)
+                tmp.append(b + c)
+                tmp.append(c + d)
+                tmp.append(d + a)
+                tmp.append(a + c)
+                tmp.append(b + d)
+                tmp.append(a+ b + c)
+                tmp.append(a+ b + d)
+                tmp.append(a + c+d)
+                tmp.append(b + c + d)
+                tmp.append(a+b+c+d) 
+                if sorted(tmp) == sorted(arr):
+                    print(a,b,c,d)
+                    exit(1)
