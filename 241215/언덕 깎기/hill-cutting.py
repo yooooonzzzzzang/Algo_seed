a137b84ae459cff0ea9629d6d517148ec2941c51
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-15 15:47:15 +0900

    Title: 언덕 깎기, Time: 59ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241215/언덕 깎기/hill-cutting.py
@@ -0,0 +1,16 @@
+# 최대랑 최소를 뽑는다
+# 이 차를 17 이내로 하면서 비용을 x * x 계산한다
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+min_cost = 100001
+for low in range(0,101):
+    high = low + 17
+    cost = 0
+
+    for h in arr:
+        if h < low:
+            cost += (low - h) ** 2
+        elif h > high:
+            cost += (h-high) ** 2
+    min_cost = min(cost, min_cost)
+print(min_cost)
