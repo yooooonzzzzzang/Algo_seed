99c09453a442c86faac2ddf72b21372ab0d372a1
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-15 14:33:23 +0900

    Title: 초기 수열 복원하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241215/초기 수열 복원하기/restore-initial-sequence.py
+++ 241215/초기 수열 복원하기/restore-initial-sequence.py
@@ -5,7 +5,10 @@ for i in range(1,n+1):
     tmp = [i]
     now = 0
     for j in range(len(arr)):
-        tmp.append(arr[j]-tmp[-1])
+        if arr[j]-tmp[-1] != 0:
+            tmp.append(arr[j]-tmp[-1])
+        else:
+            break
     if len(set(tmp)) == n:
         print(*tmp)
         break
