f9ad5ce420b99623180151369706100525167dcf
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-16 14:47:10 +0900

    Title: 구간 잘 나누기, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241216/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,19 @@
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+
+MAX_V = 10000 
+ans = MAX_V
+# i 최댓값
+for i in range(1,MAX_V+1):
+    tmp = 0
+    section = 1
+    for j in range(n):
+        if arr[j] > i:
+            break
+        if tmp + arr[j] > i:
+            tmp = 0
+            section += 1
+        tmp += arr[j]
+    if section == m:
+        ans = min(ans,i)
+print(ans)
