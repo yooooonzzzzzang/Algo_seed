577c099da37cc6b257d22d1595fda6f361b88170
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-16 18:34:36 +0900

    Title: 두 선분, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241216/두 선분/two-lines.py
+++ 241216/두 선분/two-lines.py
@@ -2,6 +2,6 @@ def check(x1,x2,x3,x4):
     flag = True
     if x2 <= x3 or x1 >= x4:
         flag = False 
-    return 'intersecting' if flag else 'nointersecting'
+    return 'intersecting' if flag else 'nonintersecting'
 
 print(check(*map(int,input().split())))
