e068657f141137a57dac0445dba044ef2ea4bda7
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-16 21:01:41 +0900

    Title: 전부 겹치는 선분, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241216/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,16 @@
+import sys
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+min_v = sys.maxsize
+max_v = -sys.maxsize
+
+for x,y in arr:
+    min_v = min(min_v, x)
+    max_v = max(max_v, y)
+
+
+if x <= y:
+    print("Yes")
+else:
+    print("No")
