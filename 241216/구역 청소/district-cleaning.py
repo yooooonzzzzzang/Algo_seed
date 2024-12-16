5606c53749f7bcc02c1529c691630a5974608b90
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-16 19:20:38 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241216/구역 청소/district-cleaning.py
+++ 241216/구역 청소/district-cleaning.py
@@ -1,9 +1,9 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
-arr = [0] * 101
 
-for i in range(a,b):
-    arr[i] = 1
-for j in range(c,d):
-    arr[j] = 1
-print(sum(arr))
+
+if b < c or d < a:
+    # 안겹침
+    print((d-c) +  (b-a))
+else:
+    print(max(b, d) - min(a,c))
