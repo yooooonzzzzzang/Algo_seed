9adaef870e149653414717190391d6734c2753b8
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-16 20:12:33 +0900

    Title: 전부 겹치는 선분 2, Time: 71ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241216/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+
+# 제외할 선분
+for k in range(n):
+    flag = "Yes"
+    for i in range(k+1, n):
+ 
+        for j in range(k+2, n):
+            x1, x2 = arr[i][0], arr[i][1]
+            x3, x4 = arr[j][0], arr[j][1]
+            # 안겹치면 No
+            if x2 < x3 or x4 < x1:
+                flag = "No"
+    if flag =="Yes":
+        break
+
+print(flag)
