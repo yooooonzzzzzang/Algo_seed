9d1f8b7920bb05872f8f20232d828cfbb7a7fd2a
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-17 11:42:42 +0900

    Title: 달리자, Time: 84ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241217/달리자/run-and-run.py
@@ -0,0 +1,12 @@
+n =int(input())
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+cnt =0
+for i in range(n):
+    if arr1[i] > arr2[i]:
+        tmp = arr1[i] - arr2[i]
+        arr1[i] -= num
+        arr1[i+1] += tmp
+        cnt += tmp
+
+print(cnt)
