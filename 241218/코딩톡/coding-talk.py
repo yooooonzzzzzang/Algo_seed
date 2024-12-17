3a063a4e7f03f3f3b051afbdbb5f84d8d229b0b8
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-18 01:05:58 +0900

    Title: 코딩톡, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241218/코딩톡/coding-talk.py
+++ 241218/코딩톡/coding-talk.py
@@ -4,5 +4,6 @@ alp = [chr(ord('A')+i) for i in range(n)]
 for i in range(p-1,n):
     if info[i][0] in alp:
         alp.remove(info[i][0])
-print(*alp)
+if info[p-1][1] != '0':
+    print(*alp)
     
