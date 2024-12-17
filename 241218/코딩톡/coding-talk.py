694b8b2f94af47706b7ef1dcb69d58824a6e2c1a
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-18 01:04:43 +0900

    Title: 코딩톡, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241218/코딩톡/coding-talk.py
@@ -0,0 +1,8 @@
+n,m,p = map(int,input().split())
+info = [input().split() for _ in range(m)]
+alp = [chr(ord('A')+i) for i in range(n)]
+for i in range(p-1,n):
+    if info[i][0] in alp:
+        alp.remove(info[i][0])
+print(*alp)
+    
