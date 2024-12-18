ca2894b98313f74f60481cc3ac42e93609ec2109
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-18 19:55:47 +0900

    Title: 연속된 숫자 만들기 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241218/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241218/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,7 +3,7 @@
 2. 처음 시작에 ACB처럼 오직 1명만 원하는 자리에 위치하는 경우라면, 1번의 교환으로 정렬이 가능하다.
 3. 처음 시작에 CAB처럼 아무도 원하는 자리에 위치하지 않는 경우라면, 2번의 교환으로 정렬이 가능하다.
 '''
-data = list(map(int, input().split()))
+data = sorted(list(map(int, input().split())))
 
 if data[0] + 1 == data[1] and data[1] + 1 == data[2]:
     print(0)
