b6f35b863d626e19ba5f6f7535e56dac4c7ea72d
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-19 16:15:10 +0900

    Title: 개발자의 가위바위보, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241219/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 241219/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -1,10 +1,65 @@
 n = int(input())
 arr = [list(map(int,input().split())) for _ in range(n)]
 cnt1 = 0
-cnt2 = 0
+ans = 0 
+# 1: 가위  2: 바위  3: 보
 for i in range(n):
-    if arr[i][0] < arr[i][1]:
-        cnt1 +=1
-    elif arr[i][0] > arr[i][1]:
-        cnt2 += 1
-print(max(cnt1, cnt2))
+    if arr[i][0] == 1 and arr[i][1] ==3:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 1:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==2:
+        cnt1 += 1
+ans = max(ans, cnt1)
+# 1: 가위  2: 보   3: 바위
+cnt1 = 0
+for i in range(n):
+    if arr[i][0] == 1 and arr[i][1] ==2:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 3:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==1:
+        cnt1 += 1
+ans = max(ans, cnt1)
+# 1: 바위  2: 가위  3: 보
+cnt1 = 0
+for i in range(n):
+    if arr[i][0] == 1 and arr[i][1] ==2:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 3:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==1:
+        cnt1 += 1
+ans = max(ans, cnt1)
+# 1: 바위  2: 보   3: 가위
+cnt1 = 0
+for i in range(n):
+    if arr[i][0] == 1 and arr[i][1] ==3:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 1:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==2:
+        cnt1 += 1
+ans = max(ans, cnt1)
+
+# 1: 보   2: 가위 3: 바위
+cnt1 = 0
+for i in range(n):
+    if arr[i][0] == 1 and arr[i][1] ==3:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 1:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==2:
+        cnt1 += 1
+ans = max(ans, cnt1)
+# 1: 보   2: 바위 3: 가위
+cnt1 = 0
+for i in range(n):
+    if arr[i][0] == 1 and arr[i][1] ==2:
+        cnt1 += 1
+    elif arr[i][0] == 2 and arr[i][0] == 3:
+        cnt1 += 1
+    elif arr[i][0] == 3 and arr[i][1] ==1:
+        cnt1 += 1
+ans = max(ans, cnt1)
+print(ans)
