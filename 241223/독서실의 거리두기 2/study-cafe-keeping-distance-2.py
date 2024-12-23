c4e3ebd0bd8b8f887b63e5f34fe8b0adbbbc7f0c
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-23 16:28:01 +0900

    Title: 독서실의 거리두기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 241223/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -4,24 +4,14 @@ max_v = 0
 for i in range(n):
     for j in range(i+1,n):
         if arr[i] == '1':
-            if arr[j] =='1' :
+            if arr[j] =='1':
                 if j-i > max_v:
                     max_v = j-i
                     max_idx = (i,j)
                 break
-max_v = 1000
-for i in range(1,n):
-    if arr[i] == '1':
-        max_v = i
-        break
-
-for i in range(n-2,-1,-1):
-    if arr[i] == '1':
-        max_v = max(max_v, n-1-i)
-        break
 
 arr[(max_idx[1] + max_idx[0])//2] = '1'
-
+max_v = 1000
 for i in range(n):
     for j in range(i+1,n):
         if arr[i] == '1':
@@ -29,6 +19,16 @@ for i in range(n):
                 if j-i < max_v:
                     max_v = j-i
                 break
-print(max_v)
 
-# 맨앞에 1 이거나 맨 뒤에 1인경우의 max 랑 ㅂㅣ교 추가 
+
+arr[(max_idx[1] + max_idx[0])//2] = '0'
+
+for i in range(1,n):
+    if arr[i] == '1' and arr[0] =='0':
+        max_v = max(max_v, i)
+        break
+for i in range(n-2, -1,-1):
+    if arr[i] == '1' and arr[-1] =='0':
+        max_v = max(max_v, n-1-i)
+        break
+print(max_v)
