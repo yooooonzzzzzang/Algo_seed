ef72005efe4dde806e2e34bed9ba00519fec71c0
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-23 20:00:19 +0900

    Title: ABC 줄 세우기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241223/ABC 줄 세우기/abc-line-up.py
+++ 241223/ABC 줄 세우기/abc-line-up.py
@@ -1,20 +1,21 @@
-def minimum_swaps_to_sort(arr):
-    # 초기화: 스왑 횟수와 배열 길이
-    swaps = 0
-    
-    # 버블 정렬 알고리즘
-    for i in range(n):
-        for j in range(0, n - i - 1):  # 범위는 점점 줄어듦
-            if arr[j] > arr[j + 1]:  # 인접 요소 비교
-                # 위치를 바꾸고 스왑 횟수 증가
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swaps += 1
-    
-    return swaps
-
-# 테스트
+# 변수 선언 및 입력:
 n = int(input())
-initial_order = list(input().split())  # 초기 순서
-result = minimum_swaps_to_sort(initial_order)
-print(result)
+arr = list(input().split())
+ans = 0
 
+# 'A' 부터 알파벳 순서대로 찾아 가장 앞으로 이동합니다.
+for i in range(n):
+    x = chr(ord('A') + i )
+
+    # i번째 알파벳을 찾아 idx에 저장합니다.
+    idx = 0
+    for j in range(n):
+        if arr[j] == x:
+            idx = j
+    
+    # idx번째 알파벳을 i번째 위치까지 swap합니다.
+    for j in range(idx - 1, i - 1, -1):
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        ans += 1
+
+print(ans)
