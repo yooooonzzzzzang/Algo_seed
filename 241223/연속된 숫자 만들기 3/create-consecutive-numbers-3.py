4f9b905bac8c68a7abc5395bb301c3a37a0204a9
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-23 14:21:49 +0900

    Title: 연속된 숫자 만들기 3, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 241223/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,8 +1,9 @@
 a,b,c = map(int,input().split())
 cnt = 0
-if c - b == 2 or b - a == 2:
-    print(1)
-elif c - b == 3 or b - a == 3:
+
+if c - b == 3 or b - a == 3:
     print(2)
+elif c - b == 2 or b - a == 2:
+    print(1)
 else:
     print(c-b-a)
