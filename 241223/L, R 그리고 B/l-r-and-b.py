78d3392fc82d6594ff07692f1a95878792cb73be
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2024-12-23 23:47:22 +0900

    Title: L, R 그리고 B, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241223/L, R 그리고 B/l-r-and-b.py
+++ 241223/L, R 그리고 B/l-r-and-b.py
@@ -13,9 +13,9 @@ for i in range(10):
             start = (i,j)
         elif arr[i][j] == 'R':
             avoid = (i,j)
-if start[0]== end[0]and end[0] == avoid[0]:
+if start[0]== end[0]and end[0] == avoid[0] and start[1] < avoid[1] and avoid[1] < end[1]:
     print(abs(end[0]-start[0]) + abs(end[1]- start[1]) +1)
-elif start[1]== end[1]and end[1] == avoid[1]:
+elif start[1]== end[1]and end[1] == avoid[1]and start[0] < avoid[0] and avoid[0] < end[0]:
     print(abs(end[0]-start[0]) + abs(end[1]- start[1]) +1)
 else:
     print(abs(end[0]-start[0]) + abs(end[1]- start[1]) -1)
