3f554bd13b6160bd391ef3db8121a261c9798963
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-01-02 14:59:14 +0900

    Title: 삽입 정렬 구현, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250102/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Write your code here!
+for i in range(1,n):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j-= 1
+    arr[j+1] = key
+print(arr)
