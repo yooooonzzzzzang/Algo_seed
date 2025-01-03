465fc7c17b03624924b206b5a8c0dbb4e2a1b2d0
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-01-03 13:14:56 +0900

    Title: 황금비율 토스트, Time: 3094ms, Memory: 189MB, Status: Time Limit Exceed - CodeTree

--- 250103/황금비율 토스트/golden-toast.java
+++ 250103/황금비율 토스트/golden-toast.java
@@ -1,12 +1,46 @@
 import java.util.ListIterator;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
 import java.util.Scanner;
 
 public class Main {
-    static ListIterator<Character> it = new ListIterator<>();
-    static int n, int m;
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine(); 
+        // LinkedList에 바로 대입
+        LinkedList<Character> l =sc.nextLine().chars()
+                                       .mapToObj(c -> (char) c)
+                                       .collect(Collectors.toCollection(LinkedList::new));
 
+        ListIterator<Character> it = l.listIterator(l.size());
+        for (int i=0; i<m; i++){
+            String input = sc.nextLine();
+
+            if (input.startsWith("L")){
+                if(it.hasPrevious()){
+                    it.previous();
+                }
+            } else if(input.startsWith("P")){
+                char k  = input.split(" ")[1].charAt(0); 
+                it.add(k);
+            } else if(input.startsWith("R")){
+                if(it.hasNext()){
+                    it.next();
+                }
+            } else{
+                if (it.hasNext()){
+                    it.next();
+                    it.remove();
+                }
+            }
+        }
+
+        // 출력 l 
+        for(char c: l){
+            System.out.print(c);
+        }
     }
 }
