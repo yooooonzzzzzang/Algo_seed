5857b563c5d7f1cc3cbe495a15718f331f4d7ed2
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-01-03 12:46:20 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250103/황금비율 토스트/golden-toast.java
@@ -0,0 +1,12 @@
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+    static ListIterator<Character> it = new ListIterator<>();
+    static int n, int m;
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
