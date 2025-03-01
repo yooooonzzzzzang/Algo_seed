d9d2b7aa459c2f575e1b9bc42f5b39b92644648a
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-03-01 15:39:28 +0900

    [level 3] Title: 기지국 설치, Time: 1.97 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,25 +1,16 @@
 import math
 def solution(n, stations, w):
     answer = 0
-    erea = []
-    for i in stations:
-        st = i-w
-        ed = i+w
-        if st < 0:
-            st = 0
-        if ed > n:
-            ed = n
-        erea.append([st,ed])
-
-    empty = []
-    start = 1
-    for s,e in erea:
-        if start < s:
-            empty.append([start, s-1])
-        start = e + 1
-    if start <= n:
-        empty.append([start,n])
     
-    for s,e in empty:
-        answer += math.ceil((e-s+1)/(2*w + 1))
+    cover = 2*w+1
+    start = 1
+    for station in stations:
+        left, right = station - w , station + w
+        if start < left:
+            answer += math.ceil((left - start ) / cover)
+        start = right + 1
+    
+    if start <= n:
+        answer += math.ceil((n-start + 1)/cover)
+        
     return answer
