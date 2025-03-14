d8a469052cbdc87bf6230f6d04a3a5a75611a63a
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-03-14 11:10:49 +0900

    Title: 강력한 폭발, Time: 54ms, Memory: 17MB, Status: Passed - CodeTree

--- 250314/강력한 폭발/strong-explosion.py
+++ 250314/강력한 폭발/strong-explosion.py
@@ -1,85 +1,52 @@
-# 변수 선언 및 입력:
 n = int(input())
-bomb_type = [
-    [0 for _ in range(n)]
-    for _ in range(n)
+grid = [list(map(int, input().split())) for _ in range(n)]
+bombs = []
+bomb_shapes = [
+    [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
+    [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],
+    [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]]
 ]
-bombed = [
-    [False for _ in range(n)]
-    for _ in range(n)
-]
 
-ans = 0
-
-bomb_pos = list()
-
-
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
-
-
-def bomb(x, y, b_type):
-    # 폭탄 종류마다 터질 위치를 미리 정의합니다.
-    bomb_shapes = [
-        [],
-        [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
-        [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],
-        [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]]
-    ]
-    
-    # 격자 내 칸에 대해서만 영역을 표시합니다.
+def bomb(x, y, type_bomb, new_arr):
     for i in range(5):
-        dx, dy = bomb_shapes[b_type][i];
-        nx, ny = x + dx, y + dy
-        if in_range(nx, ny):
-            bombed[nx][ny] = True
+        dx, dy = bomb_shapes[type_bomb][i]
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < n and 0 <= ny < n:
+            new_arr[nx][ny] = 1
 
-            
-def calc():
-    # Step1. 폭탄이 터진 위치를 표시하는 배열을
-    # 초기화합니다.
+def init():
     for i in range(n):
         for j in range(n):
-            bombed[i][j] = False
-            
-    # Step2. 각 폭탄의 타입에 따라 
-    # 초토화 되는 영역을 표시합니다.
-    for i in range(n):
-        for j in range(n):
-            if bomb_type[i][j]:
-                bomb(i, j, bomb_type[i][j])
-	
-    # Step3. 초토화된 영역의 수를 구합니다.
+            if grid[i][j] == 1:
+                bombs.append([i, j])
+
+def exploded(arr):
+    new_arr = [[0] * n for _ in range(n)]
     cnt = 0
+    for (x, y), types in zip(bombs, arr):
+        bomb(x, y, types, new_arr)
     for i in range(n):
         for j in range(n):
-            if bombed[i][j]:
+            if new_arr[i][j] == 1:
                 cnt += 1
-    
     return cnt
 
-
-def find_max_area(cnt):
-    global ans
+def recur(arr, max_area):
+    # 최적화: 현재 폭탄 배열을 완성하고 계산을 바로 하도록 합니다.
+    if len(arr) == len(bombs):
+        return max(max_area, exploded(arr))
     
-    if cnt == len(bomb_pos):
-        ans = max(ans, calc())
-        return
+    best_area = max_area  # 현재까지의 최고값을 기억
+    for i in range(3):
+        arr.append(i)
+        best_area = recur(arr, best_area)
+        arr.pop()
+    
+    return best_area
 
-    for i in range(1, 4):
-        x, y = bomb_pos[cnt]
-        
-        bomb_type[x][y] = i
-        find_max_area(cnt + 1)
-        bomb_type[x][y] = 0
-
-
-for i in range(n):
-    given_row = list(map(int, input().split()))
-    for j, bomb_place in enumerate(given_row):
-        if bomb_place:
-            bomb_pos.append((i, j))
-
-find_max_area(0)
-
+# 초기 설정 및 재귀 탐색 시작
+bombs = []
+init()
+ans = recur([], 0)
 print(ans)
