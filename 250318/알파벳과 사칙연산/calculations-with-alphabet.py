1eed73a0cf50b56017f9365e150276c399e9779c
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-03-18 21:41:03 +0900

    Title: 알파벳과 사칙연산, Time: 5066ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 250318/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,30 @@
+expression = input()
+N = len(expression)
+ol = N//2 
+nl = N-ol
+def cal(tmp):
+    global ans
+    val = tmp[0]
+    for i in range(1, nl):
+        operator = expression[2*i-1]
+        if operator == '+':
+            val += tmp[i] 
+        if operator == '*':
+            val *= tmp[i] 
+        if operator == '-':
+            val -= tmp[i] 
+
+    ans = max(ans, val)
+def recur(n):
+    if nl == n:
+        cal(arr)
+        return
+    for i in range(1,5):
+        arr.append(i)
+        recur(n+1)
+        arr.pop()
+arr = []
+ans = -int(1e9)
+recur(0)
+print(ans)
+# Please write your code here.
