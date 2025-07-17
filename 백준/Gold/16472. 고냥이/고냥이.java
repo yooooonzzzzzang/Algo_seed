34b85192ad3543b4a20763d907b3642d40cd1611
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-07-17 18:31:39 +0900

    [Gold IV] Title: 고냥이, Time: 272 ms, Memory: 21064 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,31 +1,35 @@
+
 import java.util.Scanner;
 
 public class Main {
+  static int alpCnt =0;
+  static int[] alp = new int[26];
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     char[] arr = sc.next().toCharArray();
-    int[] alp = new int[27];
     int ans = 0;
     int nextIdx = 0;
     for (int i = 0; i < arr.length; i++) {
       while (nextIdx < arr.length ){
-        alp[arr[nextIdx++] - 'a']++;
-        if (countAlp(alp) > n) break;
+        alpIncrease(arr[nextIdx++] - 'a');
+        if (alpCnt > n) break;
         ans = Math.max(ans, nextIdx - i);
-//        System.out.println(i +" " + n extIdx);
       }
-      alp[arr[i] - 'a'] --;
+      alpDecrease(arr[i] - 'a');
     }
     System.out.println(ans);
   }
-
-  private static int countAlp(int[] alp) {
-    int cnt =0;
-    for (int i = 0; i < 26; i++) {
-      if (alp[i] > 0) cnt++;
+  private static void alpIncrease(int alpidx) {
+    if (alp[alpidx]++ == 0){
+      alpCnt ++;
     }
-    return cnt;
+  }
+
+  private static void alpDecrease(int alpidx) {
+    if (alp[alpidx]-- == 1){
+      alpCnt --;
+    }
   }
 }
