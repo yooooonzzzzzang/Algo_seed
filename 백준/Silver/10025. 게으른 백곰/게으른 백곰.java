51f2ebabb0e86f883be1b3f86518659bebd0b264
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-07-18 15:43:40 +0900

    [Silver III] Title: 게으른 백곰, Time: 992 ms, Memory: 181984 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.java
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.java
@@ -1,39 +1,38 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Main
-{
-  static class Bucket {
-    int g;
-    int x;
-    Bucket(int g, int x) {
-      this.g = g;
-      this.x = x;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 얼음 양동이 수
+        int K = sc.nextInt(); // 한쪽으로 닿을 수 있는 거리
+
+        int[] arr = new int[1_000_001]; // 얼음 위치 배열
+        int maxPos = 0;
+
+        // 얼음 양동이 정보 입력
+        for (int i = 0; i < N; i++) {
+            int g = sc.nextInt(); // 얼음 양
+            int x = sc.nextInt(); // 위치
+            arr[x] = g;
+            maxPos = Math.max(maxPos, x);
+        }
+
+        int windowSize = 2 * K + 1;
+        long currSum = 0;
+        long maxSum = 0;
+
+        // 초기 윈도우 구간 합 (0 ~ windowSize - 1까지)
+        for (int i = 0; i < windowSize && i <= maxPos; i++) {
+            currSum += arr[i];
+        }
+        maxSum = currSum;
+
+        // 슬라이딩 윈도우 이동 (왼쪽에서 오른쪽으로)
+        for (int i = windowSize; i <= maxPos; i++) {
+            currSum = currSum - arr[i - windowSize] + arr[i];
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        System.out.println(maxSum);
     }
-  }
-
-  public static void main (String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    int N = sc.nextInt();
-    int K = sc.nextInt();
-    Bucket[] buckets = new Bucket[N];
-    for (int i = 0; i < N; i++)
-      buckets[i] = new Bucket(sc.nextInt(), sc.nextInt());
-    // 좌표로 정렬
-    Arrays.sort(buckets, (o1, o2) -> o1.x - o2.x);
-
-    int nextIndex = 0;
-    int maxSum = 0;
-    int currentSum = 0;
-    for (int i = 0; i < N; i++) {
-      while (nextIndex < N && buckets[nextIndex].x - buckets[i].x <= 2 * K) {
-        currentSum += buckets[nextIndex++].g;
-      }
-      maxSum = Math.max(maxSum, currentSum);
-      currentSum -= buckets[i].g;
-    }
-    System.out.println(maxSum);
-  }
 }
