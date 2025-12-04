b760bdc37ea969c1cd1b30961156ab687707054b
yj <94591534+yooooonzzzzzang@users.noreply.github.com>
2025-12-04 12:19:14 +0900

    [Gold IV] Title: 동전 1, Time: 196 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int[] coin= new int[n];
+    int[] dp = new int[k+1];
+    for (int i = 0; i < n; i++) {
+      coin[i] = sc.nextInt();
+    }
+    dp[0] = 1;
+    // 경우의 수 구하기
+    for (int i = 0; i < n; i++) {
+      for(int j = coin[i]; j<= k; j++){
+        dp[j] += dp[j - coin[i]];
+      }
+    }
+    System.out.println(dp[k]);
+  }
+}
